4da9ba14f8826405a92b40826277ac64942a4ceb
407_kyousitu <>
2024-06-28 09:45:53 +0900

    タイトル作成_分割①

---
+++ Assets/Scripts/UI/Text/TextChoices.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextChoices : MonoBehaviour
+{
+    //  Arrow 位置Y（-42、-102）
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
